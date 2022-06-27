f572733861fc935849eac799a6af08db38035cf0
MateoCata98 <mate.catalano98@gmail.com>
2022-06-27 18:18:40 -0300

    primer commit

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,19 @@
+import  { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ListadoComponent } from './listado/listado.component';
+import { HeroeComponent } from './heroe/heroe.component';
+
+@NgModule({
+    declarations: [
+        HeroeComponent,
+        ListadoComponent
+    ],
+    imports: [ 
+        CommonModule ],
+    exports: [
+        ListadoComponent
+    ],
+})
+
+export class HeroesModule {} 
